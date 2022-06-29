b2ce6cd475560638adfb72e0114428ef71ba7b52
xuan <xuancbblin@gmail.com>
2022-06-29 13:45:16 +0800

    test watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,35 @@
 <template>
   <h1>Home</h1>
+  <button @click="plusAdd">add</button>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import { watchEffect } from "vue";
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const count = ref(0);
+    const count2 = ref(0);
+    const $route = useRoute();
+
+    const plusAdd = () => {
+      count.value += 1;
+    };
+
+    watchEffect(() => {
+      console.log(count.value);
+      console.log(count2.value);
+      console.log({ ...$route });
+    });
+
+    return {
+      plusAdd,
+      count,
+      count2,
+    };
+  },
+};
 </script>
 
 <style></style>
