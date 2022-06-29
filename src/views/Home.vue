164d67507d1b35c740c8073dab463c66127f899e
xuan <xuancbblin@gmail.com>
2022-06-29 14:41:41 +0800

    pnpm install qs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,21 +7,26 @@
 import { ref } from "vue";
 import { watchEffect } from "vue";
 import { useRoute } from "vue-router";
+import qs from "qs";
 export default {
   setup() {
     const count = ref(0);
     const count2 = ref(0);
     const $route = useRoute();
+    const url = { city: "台北市", districts: "大安區", notifyCycle: "Day", notifyEnable: "true", page: "1" };
+
+    console.log(qs.stringify({ ...url }));
+    console.log(JSON.stringify({ ...url }));
 
     const plusAdd = () => {
       count.value += 1;
     };
 
-    watchEffect(() => {
-      console.log(count.value);
-      console.log(count2.value);
-      console.log({ ...$route });
-    });
+    // watchEffect(() => {
+    //   console.log(count.value);
+    //   console.log(count2.value);
+    //   console.log({ ...$route });
+    // });
 
     return {
       plusAdd,
