ee3b7183b7cd242cbea69d535efc36ae212610ad
xuan <xuancbblin@gmail.com>
2022-06-30 17:19:45 +0800

    feat: 使用dynamic component   模擬view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
-<script setup></script>
-
 <template>
-  <a href="/" :class="{ active: $route.path === '/' }">Home</a>
-  <a href="/about" :class="{ active: $route.path === '/about' }">about</a>
-  <a href="/Posts" :class="{ active: $route.path === '/Posts' }">Posts</a>
-  <!-- <router-link to="/">Home</router-link>
-  <router-link to="/about">about</router-link>
-  <router-link to="/Posts">Posts</router-link> -->
-  <!-- <router-view /> -->
+  <a href="/" @click.prevent="$router.push('/')">Home</a> |
+  <a href="/about" @click.prevent="$router.push('/about')">About</a>
+
+  <component :is="pathComponent[$route.fullPath]"></component>
 </template>
 
-<style>
-a {
-  display: inline-block;
-  margin-left: 15px;
-}
+<script>
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
 
-.active {
-  color: red;
-}
+export default {
+  components: {
+    Home,
+    About,
+  },
+  setup() {
+    const pathComponent = {
+      "/": "Home",
+      "/about": "About",
+    };
 
-/* .router-link-active {
-  color: red;
-} */
-</style>
+    return {
+      pathComponent,
+    };
+  },
+};
+</script>
+
+<style></style>
