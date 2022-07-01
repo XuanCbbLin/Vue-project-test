51cf6115dd330e0520de3ead454493d50683b479
xuan <xuancbblin@gmail.com>
2022-07-01 19:04:37 +0800

    delete {{ navigate  }}

--- src/component/RouterLinkDisable.vue
+++ src/component/RouterLinkDisable.vue
@@ -2,7 +2,6 @@
   <component :is="isDisabled ? 'span' : 'router-link'" :class="{ 'cursor-not-allowed': isDisabled }" to="/about"> About2</component> |
 
   <router-link to="/about" custom v-slot="{ navigate, href }">
-    {{ navigate }}
     <span v-if="isDisabled" class="cursor-not-allowed">about</span>
     <a v-else :href="href" @click="navigate">about</a>
   </router-link>
